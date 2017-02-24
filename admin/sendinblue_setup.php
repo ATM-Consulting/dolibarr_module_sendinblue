bd3e67a05610e092d2f225e768acdfa79324bb22
Quentin Vial-Gouteyron <quentin@mail.atm-consulting.fr>
2017-02-24 11:44:11 +0100

    fix maj f5 refreshing

--- admin/sendinblue_setup.php
+++ admin/sendinblue_setup.php
@@ -42,7 +42,7 @@ if (! $user->admin) {
 
 // Parameters
 $action = GETPOST('action', 'alpha');
-$refreshButtonPressed = isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0';
+$refreshButtonPressed = isset($_SERVER['HTTP_CACHE_CONTROL']) && ($_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0' || $_SERVER['HTTP_CACHE_CONTROL'] === 'no-cache');
 		
 
 /*
